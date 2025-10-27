20a82d0494aab826c724bb0d38e0b7a2fdc74818
Akash Rawat <rawatakash221221@gmail.com>
2025-10-27 16:06:12 +0530

    14 october

---
+++ src/ex_006_Ternary_Operator/Lab058_CLI_INPUTS.java
@@ -0,0 +1,17 @@
+package ex_006_Ternary_Operator;
+
+public class Lab058_CLI_INPUTS {
+    public static void main(String[] args) {
+        String input1=args[0];
+        String input2=args[1];
+        String input3=args[2];
+        String input4=args[3];
+        String input5=args[4];
+        System.out.println(input1);
+        System.out.println(input2);
+        System.out.println(input3);
+        System.out.println(input4);
+        System.out.println(input5);
+
+    }
+}
