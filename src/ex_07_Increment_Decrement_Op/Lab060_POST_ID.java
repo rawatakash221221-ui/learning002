da33460dacdd86122f3cddb7fd257b3633091b85
Akash Rawat <rawatakash221221@gmail.com>
2025-10-30 12:09:59 +0530

    17 october

---
+++ src/ex_07_Increment_Decrement_Op/Lab060_POST_ID.java
@@ -0,0 +1,11 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab060_POST_ID {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a++;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
---
+++ src/ex_07_Increment_Decrement_Op/Lab066_De.java
@@ -0,0 +1,11 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab066_De {
+    public static void main(String[] args) {
+        int a=10;
+        int result=--a;
+        System.out.println(result);
+        System.out.println(a);
+        System.out.println(--a);
+    }
+}
